d21f000c536dfdba263e20207bb3a4eb79fe2810
Anna Vu <ngavu2712@gmail.com>
2020-05-13 11:31:48 -0700

    move nodemon to dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var nodemon = require('nodemon');
+var nodemon = require('nodemon');
 var path = require('path');
  
 var app = express();
